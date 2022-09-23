a475a6e240ece0fffbdfd9f924c9585a8adbe527
Brodie <brodie.kirkland@my.jcu.edu>
2022-09-23 10:03:38 +1000

    Add score_menu.py with functions

---
+++ prac_02/score_menu.py
@@ -0,0 +1,37 @@
+import random
+MENU = "(G)et score\n(R)esult\n(P)rint stars\n(Q)uit"
+
+
+def main():
+    score = 0
+    print(MENU)
+    menu_choice = input(">>>")
+    while menu_choice != "Q":
+        if menu_choice == "G":
+            score = random.randint(0, 100)
+        elif menu_choice == "R":
+            return_result(score)
+        elif menu_choice == "P":
+            print_stars(score)
+    print("Fin.")
+
+
+def print_stars(score):
+    """Print stars * score"""
+    for i in range(score + 1):
+        print("*", end="")
+
+
+def return_result(score):
+    """return result"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+main()
