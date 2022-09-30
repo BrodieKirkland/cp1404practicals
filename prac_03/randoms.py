232984cc060a1a535c93e2745ad6ef0c08873766
Brodie <brodie.kirkland@my.jcu.edu>
2022-09-30 10:02:52 +1000

    Initial commit capitalist_conrad.py

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -5,7 +5,8 @@
 # What did you see on line 2?
 # 3
 # What was the smallest number you could have seen, what was the largest?
-# smallest 3, largest 10
+# smallest 3, largest 9
+# No it is counting in 2's from an odd number
 # What did you see on line 3?
 # 4.291442831596987
 # What was the smallest number you could have seen, what was the largest?
