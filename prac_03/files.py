0e5e38a126902e91cf73695f4dba00f40cf0174d
Brodie <brodie.kirkland@my.jcu.edu>
2022-09-30 13:02:58 +1000

    Add Q4

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,23 +1,32 @@
-# 1.
-
+# # 1.
+#
 name = input("Name: ")
 file_name = f"{name}.txt"
 out_file = open(file_name, "w")
 print(name, file=out_file)
 out_file.close()
 
-# 2.
-
+# # 2.
+#
 in_file = open(file_name, "r")
 print(f"Your name is {in_file.read()}")
 in_file.close()
-
-# 3.
-
+#
+# # 3.
+#
 total = 0
 in_file = open("numbers.txt", "r")
 for i in range(2):
-    line = in_file.readline()
-    total += int(line)
+    total += int(in_file.readline())
 in_file.close()
 print(f"Total from numbers.txt: {total}")
+
+# 4.
+
+total = 0
+file_name = input("File name: ")
+in_file = open(file_name, "r")
+for line in in_file:
+    total += int(line)
+in_file.close()
+print(f"Total from {file_name}: {total}")
