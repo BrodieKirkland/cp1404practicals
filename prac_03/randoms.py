6cc0b08685e3c178473a25dd0fd7bcb0542c431f
Brodie <brodie.kirkland@my.jcu.edu>
2022-09-30 10:00:47 +1000

    add answers to Q1,2,3 and random int code

---
+++ prac_03/randoms.py
@@ -0,0 +1,16 @@
+# What did you see on line 1?
+# 20
+# What was the smallest number you could have seen, what was the largest?
+# smallest 5, largest 20
+# What did you see on line 2?
+# 3
+# What was the smallest number you could have seen, what was the largest?
+# smallest 3, largest 10
+# What did you see on line 3?
+# 4.291442831596987
+# What was the smallest number you could have seen, what was the largest?
+# smallest 2.5, largest 5.5
+
+import random
+
+print(random.randint(0, 100))
