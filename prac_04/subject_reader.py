a7d9a898f812bed7c0ea8aa52ba61a675befa46d
Brodie <brodie.kirkland@my.jcu.edu>
2022-10-07 10:27:22 +1000

    Add functions

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,10 +9,13 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    print("----------")
+    print_data_string(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -22,8 +25,17 @@ def get_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
+        data.append(parts)
         print("----------")
     input_file.close()
+    return data
+
+
+def print_data_string(data):
+    """Print string from data"""
+    for i in range(len(data)):
+        print(f"{data[i][0]} is taught by {data[i][1]} and has {data[i][2]} students")
+    print("----------")
 
 
 main()
