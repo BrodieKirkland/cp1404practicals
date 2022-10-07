9c7f04f27c0166f9f6f8632eabb4e41f3ab2d03e
Brodie <brodie.kirkland@my.jcu.edu>
2022-10-07 14:17:23 +1000

    Add constants

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,16 +1,26 @@
 import random
-number_of_quick_adds = int(input("How many quick picks? "))
-quick_adds = []
-for i in range(number_of_quick_adds):
+
+NUMBER_OF_COLUMNS = 6
+MINIMUM_NUMBER = 1
+MAX_NUMBER = 45
+
+number_of_rows = int(input("How many quick picks? "))
+quick_picks = []
+for i in range(number_of_rows):
     new_line = []
-    for j in range(6):
+    for j in range(NUMBER_OF_COLUMNS):
         is_new_number = False
         while not is_new_number:
-            number = random.randint(1, 45)
+            number = random.randint(MINIMUM_NUMBER, MAX_NUMBER)
             if number not in new_line:
                 is_new_number = True
                 new_line.append(number)
         print(f"{number:2}", end=" ")
-    quick_adds.append(new_line)
-
     print()
+
+
+# for i, new_line in enumerate(quick_adds):
+#     new_line = str(new_line)
+#     print(str(quick_adds[i]))
+#     " ".join(str(quick_adds[i]))
+#     print(f"{quick_adds}")
