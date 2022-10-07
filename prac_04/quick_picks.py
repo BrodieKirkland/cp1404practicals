f2ddce410d81aedd4b54ae7cd13d995ec769d27e
Brodie <brodie.kirkland@my.jcu.edu>
2022-10-07 11:13:18 +1000

    Add is_new_number check

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,7 +1,16 @@
 import random
 number_of_quick_adds = int(input("How many quick picks? "))
-
+quick_adds = []
 for i in range(number_of_quick_adds):
+    new_line = []
     for j in range(6):
-        print(f"{random.randint(1, 45):2}", end=" ")
+        is_new_number = False
+        while not is_new_number:
+            number = random.randint(1, 45)
+            if number not in new_line:
+                is_new_number = True
+                new_line.append(number)
+        print(f"{number:2}", end=" ")
+    quick_adds.append(new_line)
+
     print()
