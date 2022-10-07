4dfa1eb50bbcf4bee41de3e524c7f737e81dc93a
Brodie <brodie.kirkland@my.jcu.edu>
2022-10-07 11:02:14 +1000

    Add for loop

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,2 +1,7 @@
 import random
-number_of_quick_adds = input("How many quick picks? ")
+number_of_quick_adds = int(input("How many quick picks? "))
+
+for i in range(number_of_quick_adds):
+    for j in range(6):
+        print(f"{random.randint(1, 45):2}", end=" ")
+    print()
