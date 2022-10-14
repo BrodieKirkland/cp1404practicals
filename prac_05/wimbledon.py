0536329d587b02b54d7e7eddb412640fe0005053
Brodie <brodie.kirkland@my.jcu.edu>
2022-10-14 14:15:03 +1000

    Add constant filename and small fixes

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,13 +1,19 @@
 """
 Time  estimate: 40 minutes
-actual: 60 minutes
+actual: 70 minutes
 """
+
+FILENAME = "wimbledon.csv"
 
 
 def main():
-    filename = "wimbledon.csv"
-    champions_data = make_champion_list(filename)
+    champions_data = make_champion_list(FILENAME)
     name_to_count, champion_countries = count_name_country(champions_data)
+    display_results(champion_countries, name_to_count)
+
+
+def display_results(champion_countries, name_to_count):
+    """ Display champions and countries"""
     print("Wimbledon Champions:")
     for name, count in name_to_count.items():
         print(f"{name} {count}")
@@ -22,16 +28,16 @@ def main():
 def count_name_country(champions_data):
     """Count name and unique countries"""
     name_to_count = {}
-    champion_countries = set()
+    countries = set()
     for data in champions_data:
         name = data[2]
         country = data[1]
-        champion_countries.add(country)
+        countries.add(country)
         try:
             name_to_count[name] += 1
         except KeyError:
             name_to_count[name] = 1
-    return name_to_count, champion_countries
+    return name_to_count, countries
 
 
 def make_champion_list(filename):
