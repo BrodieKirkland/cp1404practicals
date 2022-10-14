9df96a9a0bebc4c7baa9cbcfc10cb4f952ad97d3
Brodie <brodie.kirkland@my.jcu.edu>
2022-10-14 13:48:59 +1000

    Update emails.py with docstrings and fixes

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,19 +3,29 @@ Time estimate: 15 minutes
 actual: 35 minutes
 """
 
-email_to_name = {}
 
-email_address = input("Email: ")
-while email_address != "":
-    parts = email_address.split("@")
-    name = parts[0]
-    parts = name.split(".")
-    name = " ".join(parts).title()
-    is_correct = input(f"Is your name {name}? (Y/n) ").lower()
-    if is_correct == "n":
-        name = input("Name: ")
-    email_to_name[email_address] = name
+def main():
+    """Create dictionary of emails_to_names"""
+    email_to_name = {}
     email_address = input("Email: ")
-for email_address, name in email_to_name.items():
-    print(f"{name} ({email_address})")
+    while email_address != "":
+        name = get_name_from_email(email_address)
+        confirmation = input(f"Is your name {name}? (Y/n) ").lower()
+        if confirmation == "n":
+            name = input("Name: ")
+        email_to_name[email_address] = name
+        email_address = input("Email: ")
+    for email_address, name in email_to_name.items():
+        print(f"{name} ({email_address})")
+
+
+def get_name_from_email(email_address):
+    """Extract name from email address"""
+    prefix = email_address.split("@")[0]
+    parts = prefix.split(".")
+    name = " ".join(parts).title()
+    return name
+
+
+main()
 
