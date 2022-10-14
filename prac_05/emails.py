e653c3bf6a8d4b2fd85e6fe1db22240942aead28
Brodie <brodie.kirkland@my.jcu.edu>
2022-10-14 11:02:25 +1000

    Complete emails.py exercise

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,4 +1,21 @@
 """
 Time estimate: 15 minutes
-actual:
+actual: 35 minutes
 """
+
+email_to_name = {}
+
+email_address = input("Email: ")
+while email_address != "":
+    parts = email_address.split("@")
+    name = parts[0]
+    parts = name.split(".")
+    name = " ".join(parts).title()
+    is_correct = input(f"Is your name {name}? (Y/n) ").lower()
+    if is_correct == "n":
+        name = input("Name: ")
+    email_to_name[email_address] = name
+    email_address = input("Email: ")
+for email_address, name in email_to_name.items():
+    print(f"{name} ({email_address})")
+
