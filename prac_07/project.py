31a5ba5d6e6ee8f6136730f47b888638bafef8e6
Brodie <brodie.kirkland@my.jcu.edu>
2022-11-04 14:06:21 +1000

    Add __init__ function

---
+++ prac_07/project.py
@@ -0,0 +1,14 @@
+"""Project Class"""
+
+
+class Project:
+    """Project Class"""
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def __repr__(self):
+        pass
