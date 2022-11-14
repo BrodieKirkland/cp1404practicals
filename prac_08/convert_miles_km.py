be83237165d055d70a8bb54cc7ba0df26f27af7b
Brodie <brodie.kirkland@my.jcu.edu>
2022-11-14 13:33:47 +1000

    Add fixes

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -7,9 +7,12 @@ MILES_TO_KM = 1.60934
 
 class ConvertMilesToKm(App):
     """Convert miles to kilometres Kivy App"""
+    input_miles = StringProperty()
+
     def build(self):
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
+        self.input_miles = "(Input miles here)"
         return self.root
 
     def handle_convert_miles_to_km(self):
